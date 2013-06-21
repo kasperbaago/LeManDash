c75dbda0db8f7ed22d83bc93a6438ed0707d1790
kasperbaago <kapper14@gmail.com>
2013-06-21 06:02:11 +0200

    Working forecast data

--- jobs/lemanswhether.rb
+++ jobs/lemanswhether.rb
@@ -1,18 +1,9 @@
-require "net/http"
-require "uri"
-require 'digest/md5'
-require 'time'
+require 'forecast_io'
 
-oldCheckSum = 0;
+Forecast::IO.api_key = '0f98af51949d8c23ca31f35b968568b4'
 
 # :first_in sets how long it takes before the job is first run. In this case, it is run immediately
 SCHEDULER.every '2m', :first_in => 0 do |job|
-    r = Time.now.to_i;
-    response = Net::HTTP.get_response("api.forecast.io","forecast/0f98af51949d8c23ca31f35b968568b4/47.957402,0.214105?cache=" + r.to_s)
-    checksum = Digest::MD5.hexdigest(response.body);
-
-    if checksum != oldCheckSum
-        send_event('lmwhetherdata',{data: response.body});
-        oldCheckSum = checksum;
-    end
+    forecast = Forecast::IO.forecast(37.8267, -122.423);
+    send_event('lmwhetherdata',{data: forecast.currently});
 end
