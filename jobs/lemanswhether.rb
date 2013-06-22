b61e7b2ba36763a0b8cbe1085ca9232da0b6155a
kasperbaago <kapper14@gmail.com>
2013-06-22 11:11:04 +0200

    Client check of data and smaller tiles

--- jobs/lemanswhether.rb
+++ jobs/lemanswhether.rb
@@ -4,6 +4,6 @@ Forecast::IO.api_key = '0f98af51949d8c23ca31f35b968568b4'
 
 # :first_in sets how long it takes before the job is first run. In this case, it is run immediately
 SCHEDULER.every '2m', :first_in => 0 do |job|
-    forecast = Forecast::IO.forecast(37.8267, -122.423);
+    forecast = Forecast::IO.forecast(47.957402, 0.214105);
     send_event('lmwhetherdata',{data: forecast.currently});
 end
